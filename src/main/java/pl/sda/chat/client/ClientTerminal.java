63477989f0c4d20f1a6af6b8426e351575c35038
Łukasz Suma <lukaszsuma91@gmail.com>
2018-11-25 11:43:44 +0100

    add ClientTerminalMain

--- src/main/java/pl/sda/chat/client/ClientTerminal.java
+++ src/main/java/pl/sda/chat/client/ClientTerminal.java
@@ -4,7 +4,6 @@ import pl.sda.chat.model.ChatMessage;
 
 import java.io.IOException;
 import java.io.ObjectOutputStream;
-import java.io.OutputStream;
 import java.net.Socket;
 import java.util.Scanner;
 
---
+++ src/main/java/pl/sda/chat/client/ClientTerminalMain.java
@@ -0,0 +1,22 @@
+package pl.sda.chat.client;
+
+import java.io.IOException;
+
+public class ClientTerminalMain {
+    public static void main(String[] args) {
+        try {
+            ClientTerminal clientTerminal = new ClientTerminal();
+            Thread thread = new Thread(clientTerminal);
+            thread.setName("Temrinal");
+            thread.start();
+
+            thread.join();
+        } catch (IOException e) {
+            System.out.println("Could not connect to server: " + e.getMessage());
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("Application was forcibly closed");
+            e.printStackTrace();
+        }
+    }
+}
